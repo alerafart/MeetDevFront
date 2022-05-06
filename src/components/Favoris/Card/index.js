7b7e8872e55104eb763a2c07e2101bcea1782649
SebastienWilk <wilk.sebastien@gmail.com>
2022-05-06 14:17:18 +0200

    feat(view):favorite page is done

---
+++ src/components/Favoris/Card/index.js
@@ -0,0 +1,26 @@
+import './card.scss';
+import men from '../../../assets/images/men.png';
+import javascript from '../../../assets/images/javascript.png';
+
+function Card() {
+  return (
+
+    <div className="card">
+      <div className="card__avatar">
+        <img src={javascript} alt="Avatar" className="card__avatar--logo" />
+        <img src={men} alt="Avatar" className="card__avatar--img" />
+      </div>
+      <div className="card__container">
+        <h4 className="card__container--name">Jean Aimarre</h4>
+        <p className="card__container--localisation">Lyon</p>
+        <p className="card__container--experience">Experience <span className="card__container--experience--year"> -1 an</span></p>
+      </div>
+      <div className="card__button">
+        <button type="button" className="card__button--profileview">Voir le profil</button>
+        <button type="button" className="card__button--delete">supprimer</button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/components/Favoris/index.js
+++ src/components/Favoris/index.js
@@ -1,10 +1,19 @@
 // styles
 import './favoris.scss';
+import Card from './Card';
 
 function Favoris() {
   return (
     <div className="favoris">
-      favoris
+      <h1 className="favoris__title"> Mes profils favoris </h1>
+      <div className="favoris__card">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+
+      </div>
     </div>
   );
 }

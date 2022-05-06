c6ae02383fda8fe9fe4ba6cb0668f03b5d8f05e1
SebastienWilk <wilk.sebastien@gmail.com>
2022-05-06 15:03:31 +0200

    recruiter profile init

---
+++ src/components/RecruiterProfil/index.js
@@ -0,0 +1,83 @@
+import './recruiterprofil.scss';
+
+import Agathe from '../../assets/images/agathe-feeling.jpg';
+
+function RecruiterProfile() {
+  return (
+    <div className="profil">
+      <div className="profil__leftpart">
+        <img
+          className="profil__picture__img"
+          src={Agathe}
+          alt="profil"
+        />
+
+        <h3 className="profil__informations--name">Agathe Feeling</h3>
+        <p className="profil__informations--localisation">Lyon</p>
+
+        <p className="profil__informations--aboutme">About me</p>
+        <p className="profil__informations--description"> "I gotta feeling WOOHOO"</p>
+        <p className="profil__informations--email">agat.feeling@woohoo.com</p>
+        <p className="profil__informations--phone">0607080910</p>
+      </div>
+
+      <div className="profil__rightpart">
+
+        <div className="profil__informations--checkbox">
+          <label htmlFor="switch" className="switch">
+            <input type="checkbox" checked />
+            <span className="slider round" />
+          </label>
+          <p className="profil__informations--visibility">Profil visible</p>
+        </div>
+        <div className="profil__informations--selectedlanguages">
+          <p className="profil__informations--string"> Langages pratiqués </p>
+          <ul className="profil__informations--languages">
+            <li className="profil__informations--language"><img alt="logo html" src={html} className="profil__informations--language--picture" /></li>
+            <li className="profil__informations--language"><img alt="logo php" src={php} className="profil__informations--language--picture" /></li>
+            <li className="profil__informations--language"><img alt="logo css" src={css} className="profil__informations--language--picture" /></li>
+            <li className="profil__informations--language"><img alt="logo js" src={js} className="profil__informations--language--picture" /></li>
+
+          </ul>
+        </div>
+        <div className="profil__informations--experiences">
+          <p className="profil__informations--experience">Expérience</p>
+          <p className="profil__informations--experience">1 à 3 ans</p>
+        </div>
+        <div className="profil__informations--site">
+          <div className="profil__informations__github">
+            <img
+              className="profil__informations__github--img"
+              src={github}
+              alt="github logo"
+            />
+            <p className="profil__informations__github--link"><a htmlFor="link" target="blank" href="github.com/agathe-feeling/">github.com/agathe-feeling/</a></p>
+          </div>
+          <div className="profil__informations__othersite">
+            <img
+              className="profil__informations__othersite--img"
+              src={portfolio}
+              alt="portfolio logo"
+            />
+            <p className="profil__informations__othersite--link"><a htmlFor="link" target="blank" href="agathe-feeling.fr">agathe-feeling.fr</a></p>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="profil__modify">
+
+        <button
+          className="profil__modify__form"
+          type="button"
+          // onClick={() => setModifyInformation(true)}
+        >
+          Modifier mes informations
+        </button>
+
+      </div>
+    </div>
+  );
+}
+
+export default RecruiterProfile;

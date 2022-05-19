8fd7d1653d6192765377957e85ef18a9e48d346f
HenriTeinturier <henri.teinturier@gmail.com>
2022-05-19 19:22:34 +0200

    feat(jwt) bearer seams ok but error back with data?

--- src/middlewares/ModifyDevApi.js
+++ src/middlewares/ModifyDevApi.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { VALIDATE_MODIFY_DEV } from '../actions/middleware';
-import { majProfilDevFromApi } from '../actions/profilDev';
 
 const ModifyDevApi = (store) => (next) => (action) => {
   switch (action.type) {
@@ -26,16 +25,19 @@ const ModifyDevApi = (store) => (next) => (action) => {
         age,
       } = state.profilDevModifyTemp.register;
       const userId = state.settings.log.user_id;
-
+      const { token } = state.settings.log;
+      console.log(token);
       // Transformation du tableau de technology reçu en string:
       const technologyInString = technology.toString();
 
-      // TODOO ligne 34 mettre en dynamqiue l'id user
-      // TODO RECHARGER PROFIL DEV
-      axios.put(
-        `http://aliciamv-server.eddi.cloud/projet-10-meet-dev-back/public/api/secure/users/${userId}`,
-        // ou url: 'http://localhost/api/users:8000',
-        {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const params = {
+        params: {
           lastname: lastname,
           firstname: firstname,
           city: city,
@@ -59,20 +61,57 @@ const ModifyDevApi = (store) => (next) => (action) => {
           other_link: '', // non pris en compte
           language: '', // non pris en compte: Obligatoire?
         },
-      )
-        .then((response) => {
-          console.log(response.data);
-          console.log('modification envoyé');
-          // TODO BACK VA COIRRIGER POUR ME RENVOYER LES INFOS AVEC LA REPONSE
-          store.dispatch(majProfilDevFromApi(response.data));
-        }).catch((error) => {
-          console.log(error);
-          console.log('modification echec');
-        });
+      };
+
+      const url = `http://aliciamv-server.eddi.cloud/projet-10-meet-dev-back/public/api/secure/users/${userId}`;
+      console.log('ligne 68');
+      axios.put(url, params, config,
+        // TODOO ligne 34 mettre en dynamqiue l'id user
+        // TODO RECHARGER PROFIL DEV
+        // axios.put(
+        //   `http://aliciamv-server.eddi.cloud/projet-10-meet-dev-back/public/api/secure/users/${userId}`,
+        //   // ou url: 'http://localhost/api/users:8000',
+        //   {
+        //     headers: {
+        //       Authorization: `Bearer ${token}`,
+        //     },
+        //     lastname: lastname,
+        //     firstname: firstname,
+        //     city: city,
+        //     zip_code: zipCode,
+        //     department: departement,
+        //     phone: phone,
+        //     subscribe_to_push_notif: 0,
+        //     profile_picture: profilePicture,
+        //     label: label,
+        //     description: description,
+        //     available_for_recruiters: availability,
+        //     available_for_developers: availability, // pour le moment dispo pas différencié
+        //     minimum_salary_requested: salary,
+        //     // maximum_salary_requested: 0,
+        //     age: age, // non pris en comtpe
+        //     languages: technologyInString,
+        //     years_of_experience: experience,
+        //     english_spoken: english,
+        //     github_link: github,
+        //     portfolio_link: portfolio,
+        //     other_link: '', // non pris en compte
+        //     language: '', // non pris en compte: Obligatoire?
+        //   },
+      ).then((response) => {
+        console.log(response.data);
+        console.log('modification envoyé');
+        // TODO BACK VA COIRRIGER POUR ME RENVOYER LES INFOS AVEC LA REPONSE
+      }).catch((error) => {
+        console.log(error);
+        console.log('modification echec');
+      });
+      console.log('ligne 114');
       next(action);
       break;
     }
     default:
+      console.log('ligne 119');
       next(action);
   }
 };

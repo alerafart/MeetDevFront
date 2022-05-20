d32746aee2c38ef6353953c15200bbde169ed955
christophe-brocard <brocard.c@gmail.com>
2022-05-20 10:56:38 +0200

    feat(view) change mail and add div

--- src/components/ModifyRecruiter/index.js
+++ src/components/ModifyRecruiter/index.js
@@ -132,7 +132,8 @@ function ModifyRecruiter() {
           <div className="inscriptionRecruiter__form__champ--label">
             Mail
           </div>
-          <input className="inscriptionRecruiter__form__champ--input" type="email" value={data.description} name="email" onChange={handleChangeForm} />
+          <input className="inscriptionRecruiter__form__champ--input" type="email" value={data.mail} name="email" onChange={handleChangeForm} />
+        </div>
         <div className="inscriptionRecruiter__form__champ">
           <div className="inscriptionRecruiter__form__champ--label">
             Description

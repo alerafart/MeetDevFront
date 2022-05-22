fa6fa1ed3a86120d7ca2395281bea655f700f421
HenriTeinturier <henri.teinturier@gmail.com>
2022-05-22 23:50:22 +0200

    htacess with redirect done with routerdom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-// import { BrowserRouter as Router } from 'react-router-dom';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+// import { HashRouter as Router } from 'react-router-dom';
 
 import './styles/index.scss';
 import store from './store';
